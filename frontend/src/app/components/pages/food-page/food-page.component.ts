3e4594a891c64d00becf0af4e79e07fe59315e6c
Prabodashan <prabodashan14@gmail.com>
2023-06-14 19:03:32 +0530

    Added Cart Page Functions

--- frontend/src/app/components/pages/food-page/food-page.component.ts
+++ frontend/src/app/components/pages/food-page/food-page.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { Food } from 'src/app/shared/models/Food';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { FoodService } from 'src/app/services/food.service';
+import { CartService } from 'src/app/services/cart.service';
 
 @Component({
   selector: 'app-food-page',
@@ -10,7 +11,12 @@ import { FoodService } from 'src/app/services/food.service';
 })
 export class FoodPageComponent implements OnInit {
   food!: Food;
-  constructor(activatedRoute: ActivatedRoute, foodService: FoodService) {
+  constructor(
+    activatedRoute: ActivatedRoute,
+    foodService: FoodService,
+    private cartService: CartService,
+    private router: Router
+  ) {
     activatedRoute.params.subscribe((params) => {
       if (params.id) {
         this.food = foodService.getFoodById(params.id);
@@ -19,4 +25,9 @@ export class FoodPageComponent implements OnInit {
   }
 
   ngOnInit(): void {}
+
+  addToCart() {
+    this.cartService.addToCart(this.food);
+    this.router.navigateByUrl('/cart-page');
+  }
 }
